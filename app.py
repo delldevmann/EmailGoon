127965d59e717b929de691384b7c58f8ab651ee8
Ralph <ralphsmashthecomputer@gmail.com>
2024-09-10 07:59:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,22 @@ import json
 class EmailHarvester:
     def __init__(self):
         self.visited_urls: Set[str] = set()
-        self.email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+        self.email_pattern = re.compile(r'''
+            (?i)                             # Case insensitive
+            (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+ # Local part (before the @)
+              (?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*  # Allow for dot-separated names
+            |"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*") # Quoted strings
+            )
+            @                               # At symbol
+            (?:
+                (?:(?:[a-z0-9\x80-\uFFFF](?:[a-z0-9-]*[a-z0-9\x80-\uFFFF])?\.)+[a-z\x80-\uFFFF]{2,}) # Domain part with Unicode
+            |   \[(?:                     # Or an IP address or domain literal
+                (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}
+                (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:
+                (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+) # Literal part
+                \])
+            )
+        ''', re.VERBOSE)
         self.errors: Dict[str, str] = {}  # Dictionary to store errors
 
     async def fetch_url(self, session: aiohttp.ClientSession, url: str) -> str:
